8e95daeec37e8a3fc1186b9d8acca0809d36270c
Michio Kenneth <kenn.michio@gmail.com>
2019-12-17 03:26:16 +0000

    Edited the connection file

--- models/connection.js
+++ models/connection.js
@@ -4,11 +4,11 @@
  const connection = () => {
 
      const connection = mysql.createConnection({
-         host: "remotemysql.com",
-         user: "tvoXpSQy4q",
-         password: "xEWRbca5OT",
-         database: "tvoXpSQy4q",
-         port: 3306
+         host: process.env.DB_HOST,
+         user: process.env.DB_USER,
+         password: process.env.DB_PASS,
+         database: process.env.DB_NAME,
+         port: process.env.DB_PORT
      });
 
      return {
